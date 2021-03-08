97ca9d234a8c02a24a7a8a1f7400887d23384e55
cottonstartiet <cottonstartiet@gmail.com>
2021-03-08 12:49:16 +0530

    Tree node

---
+++ Models/TreeNode.cs
@@ -0,0 +1,14 @@
+namespace CodingPractice.Models
+{
+    public class TreeNode
+    {
+        public int Value { get; set; }
+        public TreeNode Left { get; set; }
+        public TreeNode Right { get; set; }
+
+        public TreeNode(int value)
+        {
+            Value = value;
+        }
+    }
+}
